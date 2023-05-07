df17f7cd7b300b7f78af2c3cf0d83252b27c3236
Antonii Georgiev <tonygeorgiew@gmail.com>
2023-05-07 10:10:15 +0300

    Initial Backend Architecture

---
+++ PaymentSystem/tests/integration-tests/PaymentSystem.API.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace PaymentSystem.API.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ PaymentSystem/tests/unit-tests/PaymentSystem.Application.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace PaymentSystem.Application.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
